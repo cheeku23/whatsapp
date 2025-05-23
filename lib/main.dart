7887cc60d0d0739b6462fea124ecc254c533b932
cheeku23 <hassantariq9896@example.com>
2025-05-23 15:21:39 +0500

    23 May

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp/firstSceen.dart';
+
+import 'package:whatsapp/seconcl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const FirstApp(),
+      home: const MyPractice(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/seconcl.dart
+++ lib/seconcl.dart
@@ -1,10 +1,71 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class MyPractice extends StatelessWidget {
+  const MyPractice({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(); //
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green.shade800,
+        title: Text("Whatsapp"),
+        actions: [
+          SizedBox(width: 5),
+          Icon(Icons.camera_alt),
+          SizedBox(width: 5),
+
+          Icon(Icons.call),
+          SizedBox(width: 5),
+          Icon(Icons.more_vert),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(width: 10),
+              Icon(Icons.person, size: 30),
+              SizedBox(width: 10),
+              Text(
+                "MY chat",
+                style: TextStyle(color: Colors.amber, fontSize: 20),
+              ),
+            ],
+          ),
+          SizedBox(width: 20),
+
+          SizedBox(height: 20),
+
+          Row(
+            children: [
+              SizedBox(width: 10),
+              Icon(Icons.person_2, size: 30),
+              Text(
+                "Muskan e Jhooti Hain",
+                style: TextStyle(color: Colors.black, fontSize: 20),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+
+          Row(
+            children: [
+              SizedBox(width: 10),
+              Icon(Icons.person_3, color: Colors.brown, size: 30),
+              SizedBox(width: 10),
+              Text(
+                "Wasay Hashmi",
+                style: TextStyle(color: Colors.blue, fontSize: 20),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
